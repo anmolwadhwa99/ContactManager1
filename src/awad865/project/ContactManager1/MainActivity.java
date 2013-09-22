084464b53c247f9aab4c089034549d28d813ffaf
Anmol Wadhwa <anmolwadhwa99@gmail.com>
2013-09-22 21:42:22 +1200

    Displayed alist of contacts in the listview with a custom layout

--- src/awad865/project/ContactManager1/MainActivity.java
+++ src/awad865/project/ContactManager1/MainActivity.java
@@ -4,18 +4,22 @@ package awad865.project.ContactManager1;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.contactmanager1.R;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.contactmanager1.R;
 
 
 public class MainActivity extends Activity {
@@ -43,15 +47,52 @@ public class MainActivity extends Activity {
 	}
 
 	private void setUpListView(){
-		List <String> displayList = new ArrayList<String>();
-		displayList.add("Display Item 1");
-		displayList.add("Display Item 2");
-		displayList.add("Display Item 3");
+		List <Contact> displayList = new ArrayList<Contact>();
+		displayList.add(new Contact("Anmol","Wadhwa","53743632"));
+		displayList.add(new Contact("Juhi","Goswami","4234232"));
+		displayList.add(new Contact("Laurence","Baldwick","243232"));
 
 
-		ListAdapter listAdapter = new ArrayAdapter<String>(MainActivity.this,
-				android.R.layout.simple_list_item_1,displayList);
+
+		ListAdapter listAdapter = new CustomListAdapter(MainActivity.this,displayList);
 		listView.setAdapter(listAdapter);
+
+	}
+
+	private class CustomListAdapter extends ArrayAdapter<Contact>{
+
+		private Context _context;
+		private List<Contact> _contacts;
+
+		public CustomListAdapter(Context context, List<Contact> contacts){
+			super(context,android.R.layout.simple_list_item_1,contacts);
+
+			_context = context;
+			_contacts = contacts;
+
+		}
+
+		public View getView(int position, View convertView,ViewGroup parent){
+			//Create a layout inflater to inflate our xml layout for each item in the list
+			LayoutInflater inflater = (LayoutInflater) _context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			//Inflate the list item layout. Keep a reference to the inflated view.
+			//No root view specified
+			View listItemView = inflater.inflate(R.layout.custom_list_item_layout,null);
+
+			//Access TextView elements inside the view (note we must specify the parent view
+			//to look in)
+			TextView firstName =  (TextView)listItemView.findViewById(R.id.list_item_firstname);
+			TextView lastName =  (TextView)listItemView.findViewById(R.id.list_item_lastname);
+			TextView number =  (TextView)listItemView.findViewById(R.id.list_item_number);
+
+			//Set the text for each textview (use the position arugment to find the appropriate element in the list)
+			firstName.setText(_contacts.get(position).getFirstName());
+			lastName.setText(_contacts.get(position).getLastName());
+			number.setText(_contacts.get(position).getNumber());
+
+			return listItemView;
+		}
 
 	}
 
